928804357f141186d24e111e2b6f729ecb041f51
Maxlego08 <maxlego08@sfr.fr>
2025-03-18 15:26:17 +0100

    :construction: Remove logger and use listener

--- src/main/java/fr/maxlego08/superiorskybloc/ZMenuModule.java
+++ src/main/java/fr/maxlego08/superiorskybloc/ZMenuModule.java
@@ -26,17 +26,16 @@ public final class ZMenuModule extends PluginModule {
 
     @Override
     public void onReload(SuperiorSkyblock superiorSkyblock) {
-        System.out.println("Oui je reload bien le module zMenu !");
+        menusProvider.getMenuManager().loadInventories();
     }
 
     @Override
     public void onDisable(SuperiorSkyblock superiorSkyblock) {
-        System.out.println("Oui je disable bien le module zMenu !");
     }
 
     @Override
     public Listener[] getModuleListeners(SuperiorSkyblock superiorSkyblock) {
-        return new Listener[0];
+        return new Listener[]{this.menusProvider.getMenuManager()};
     }
 
     @Override
