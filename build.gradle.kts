414792f294488f659937ad15d44881c2c54e40b2
Maxlego08 <maxlego08@sfr.fr>
2025-06-23 14:55:35 +0200

    :memo: Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,17 @@ repositories {
     maven(url = "https://repo.papermc.io/repository/maven-public/")
     maven(url = "https://oss.sonatype.org/content/groups/public/")
     maven(url = "https://repo.extendedclip.com/releases/")
+    maven {
+        name = "groupezReleases"
+        url = uri("https://repo.groupez.dev/releases")
+    }
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    compileOnly(files("libs/SuperiorSkyblock2-2024.4-b445.jar"))
-    compileOnly(files("libs/zMenu-API-1.1.0.0.jar"))
     compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("fr.maxlego08.menu:zmenu-api:1.1.0.0")
+    compileOnly(files("libs/SuperiorSkyblock2-2025.1-b526.jar"))
 }
 
 tasks.compileJava {
